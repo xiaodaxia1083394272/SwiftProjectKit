f67dd7b9e0283e59699178ab3eff8c9da5e9f25f
xiaoyang <1083394272@qq.com>
2018-03-15 00:24:30 +0800

    新文件

---
+++ SwiftProjectKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftProjectKit
+//
+//  Created by HMRL on 2018/3/15.
+//  Copyright © 2018年 HMRL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
