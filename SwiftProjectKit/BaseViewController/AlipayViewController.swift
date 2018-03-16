5daf4db71bafd63f9515df0ae7884b29edcd48d6
xiaoyang <1083394272@qq.com>
2018-03-16 18:43:56 +0800

    cell

--- SwiftProjectKit/BaseViewController/AlipayViewController.swift
+++ SwiftProjectKit/BaseViewController/AlipayViewController.swift
@@ -8,28 +8,41 @@
 
 import UIKit
 
-class AlipayViewController: BaseViewController {
-
+class AlipayViewController: BaseViewController ,UITableViewDelegate,UITableViewDataSource{
+    
+    
+    fileprivate lazy var tableView:UITableView = {[unowned self] in
+        //
+        let tableView = UITableView(frame: self.view.bounds)
+        tableView.delegate = self as? UITableViewDelegate
+        tableView.dataSource = self as? UITableViewDataSource
+        return tableView
+        
+    }()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        view.addSubview(tableView)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return 5
     }
-    */
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //pss_注1，习惯在声明前加特定的类型
+         var cell = tableView .dequeueReusableCell(withIdentifier: "memeda")
+        if ((cell) != nil){
+            cell = UITableViewCell();
+        }
+        return cell!;
+    }
 
+    //
+    deinit {
+        
+    }
 }
---
+++ SwiftProjectKit/Main/Header.h
@@ -0,0 +1,22 @@
+//
+//  Header.h
+//  SwiftProjectKit
+//
+//  Created by HMRL on 2018/3/16.
+//  Copyright © 2018年 HMRL. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+//注， 疑
+#define kTextView   @"自定义textView"
+#define kTabBar     @"tabbar模板"
+#define kBuffer     @"毛玻璃"
+#define kAnimate    @"动画"
+#define kMJExtention  @"MJExtention的一般用法"
+#define kTime         @"倒计时"
+#define kPhotoBrower  @"有删除功能的图片浏览器(XLPhotoBrowser)"
+
+
+#endif /* Header_h */
